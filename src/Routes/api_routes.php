74294bb514a05ccb6c9c2b1fb0b09e27463e219a
Gretron <89054564+Gretron@users.noreply.github.com>
2023-03-29 15:55:21 -0700

    chore(src): rename AboutController to RootController

--- src/Routes/api_routes.php
+++ src/Routes/api_routes.php
@@ -2,7 +2,7 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Vanier\Api\Controllers\AboutController;
+use Vanier\Api\Controllers\RootController;
 
 // Import the app instance into this file's scope.
 global $app;
@@ -12,10 +12,10 @@ global $app;
 // The Vanier\Api must be used as namespace prefix. 
 
 // ROUTE: /
-$app->get('/', [AboutController::class, 'handleAboutApi']); 
+$app->get('/', [RootController::class, 'handleGetRoot']); 
 
 // ROUTE: /hello
 $app->get('/hello', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Reporting! Hello there!");    
     return $response;
-});
+}); 
--- src/controllers/RootController.php
+++ src/controllers/RootController.php
@@ -6,9 +6,9 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Vanier\Api\Helpers\Validator;
 
-class AboutController extends BaseController
+class RootController extends BaseController
 {
-    public function handleAboutApi(Request $request, Response $response, array $uri_args)
+    public function handleGetRoot(Request $request, Response $response, array $uri_args)
     {
         $data = array(
             'about' => 'Welcome, this is a Web services that provides this and that...',
